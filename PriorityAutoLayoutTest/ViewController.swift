0694430d23cf752274482a8e35611c8a9d2b9b90
byungkook hwang <bkhwang82@gmail.com>
2017-05-17 17:00:04 +0900

    no message

---
+++ PriorityAutoLayoutTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PriorityAutoLayoutTest
+//
+//  Created by 황병국 on 17/05/2017.
+//  Copyright © 2017 quse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
